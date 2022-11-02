7f16c04faa948ae61e328ca154b15ca0c2aa85f5
Weston Sandfort <sandfortw@gmail.com>
2022-11-02 13:35:49 -0600

    Initial commit

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,30 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "Is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers equal to string_teachers?", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "Is number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "Is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "Is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
-# this should print: true
+puts "Is number_students less than or equal to 20?", number_students <= 20
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "Is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -52,22 +58,34 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Be as technically precise as possible, but don't just copy and paste a definition from the readings.
 # Make sure YOU can explain it that way!
 
+puts "4 < 9"
 puts 4 < 9
 #YOU DO: Explain.
 
+#THIS WILL PRINT THE RESULT OF THE COMPARISON "4 < 9." SINCE IT IS TRUE, IT SHOULD PRINT TRUE AND GO TO THE NEXT LINE.
+
+puts "4 < books (3)"
 books = 3
 puts 4 < books
 # YOU DO: Explain.
 
+#THIS WILL PRINT THE RESULT OF THE COMPARION "IS FOUR LESS THAN 'books' " SINCE "books" IS 3, THIS WILL PRINT FALSE.
+
+puts "friends (6) greater than siblings (2)?"
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
 
+#AS ABOVE, THIS WILL PRINT THE RESULT OF THE STATEMENT "IS FRIENDS GREATER THAN SIBLINGS." SINCE THE NUMBER OF FRIENDS IS GREATER THAN THE NUMBER OF SIBLINGS, THIS SHOULD PRINT TRUE.
+
+puts "Attendees (9) not equal to meals (8)?"
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+
+#THIS PRINTS THE RESULT OF THE COMPARISON, "IS ATTENDEES NOT EQUAL TO THE NUMBER OF MEALS." SINCE ATTENDEES IS 9 AND MEALS IS 8, THIS WILL PRINT "TRUE."
 
 
 #-------------------
@@ -90,15 +108,23 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
+puts "Loves to play and loves treats?"
+puts loves_to_play and loves_treats
 
 
 # Determine if the dog loves to play and loves the dog park
+puts "Loves to play and loves dog park?"
+puts loves_to_play and loves_dog_park
 
 
 # Determine if the dog loves to play or loves the dog park
+puts "Loves to play or loves dog park?"
+puts loves_to_play or loves_dog_park
 
 
 # Determine if the dog loves to play and is a puppy
+puts "Loves to play and is puppy?"
+puts loves_to_play && age <= 1
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: The final line of code evaulated to true because I decided a puppy is a dog that is less than or equal to 1.
--- self_eval.md
+++ self_eval.md
@@ -1,8 +1,16 @@
 # Self Evaluation
 
 1. How confident do you feel in your understanding and fluency with the comparison operators?
+5/5
 1. How confident do you feel in your understanding and fluency with the logical operators?
+5/5
 1. How confident do you feel in your understanding and fluency with the if statements?
+4/5
 1. How much time did you spend on this lesson/topic? How did that line up with your time estimate?
+An hour and a half. I thought it would take me an hour, but I ran into a few bugs.
+
 1. What do you still need to practice or learn? How will you do that? Do you need to adjust your calendar in order to do that?
+I want to do challenge 3, and mess with even/odds again using the "%" function instead.
+
 1. What questions do you still have? How will you get the answers you need?
+None right now, I will try to get them myself through google, then go to my work group
